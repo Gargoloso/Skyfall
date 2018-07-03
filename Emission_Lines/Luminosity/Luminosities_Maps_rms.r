b1d3c26dac5489b9f4539df0c7a794b8ecc439ec
Gargoloso <aitorrobleto@gmail.com>
2018-07-03 18:05:31 -0500

    Added luminosity scripts for the Base lines. Plots only H-alpha, [OIII] 5007 and [N II] 6583 maps. The rms version includes criteria for filtering out spaxels based ond their relative errors (in the fluxes, not luminosities). Distance determination errors pending.

--- Emission_Lines/Luminosity/Luminosities_Maps_rms.r
+++ Emission_Lines/Luminosity/Luminosities_Maps_rms.r
@@ -119,20 +119,42 @@ X <- x - xc
 ##Convert from surface specific flux to standard specific intensity [erg cm^-2 s^-1 A^-1 sr^-1]
 
 print('Converting surface specific units to specific intensities.')
-fa   <- FA/2.3504e-11 
-fn1  <- FN1/2.3504e-11 
-fn   <- FN/2.3504e-11 
-fb   <- FB/2.3504e-11
-fo1  <- FO1/2.3504e-11
-fo   <- FO/2.3504e-11 
+fa   <- FA[which(rms_FA/FA <= 1.0)]/2.3504e-11 
+fn1  <- FN1[which(rms_FN1/FN1 <= 1.0)]/2.3504e-11 
+fn   <- FN[which(rms_FN/FN <= 1.0)]/2.3504e-11 
+fb   <- FB[which(rms_FB/FB <= 1.0)]/2.3504e-11
+fo1  <- FO1[which(rms_FO1/FO1 <= 1.0)]/2.3504e-11
+fo   <- FO[which(rms_FO/FO <= 1.0)]/2.3504e-11 
 
+XA  <- X[which(rms_FA/FA <= 1.0)]
+YA  <- Y[which(rms_FA/FA <= 1.0)]
+IDA <- ID2[which(rms_FA/FA <= 1.0)]
+
+XN1  <- X[which(rms_FN1/FN1 <= 1.0)]
+YN1  <- Y[which(rms_FN1/FN1 <= 1.0)]
+IDN1 <- ID2[which(rms_FN1/FN1 <= 1.0)]
+
+XN  <- X[which(rms_FN/FN <= 1.0)]
+YN  <- Y[which(rms_FN/FN <= 1.0)]
+IDN <- ID2[which(rms_FN/FN <= 1.0)]
+
+XB  <- X[which(rms_FB/FB <= 1.0)]
+YB  <- Y[which(rms_FB/FB <= 1.0)]
+IDB <- ID2[which(rms_FB/FB <= 1.0)]
+
+XO1  <- X[which(rms_FO1/FO1 <= 1.0)]
+YO1  <- Y[which(rms_FO1/FO1 <= 1.0)]
+IDO1 <- ID2[which(rms_FO1/FO1 <= 1.0)]
+
+XO  <- X[which(rms_FO/FO <= 1.0)]
+YO  <- Y[which(rms_FO/FO <= 1.0)]
+IDO <- ID2[which(rms_FO/FO <= 1.0)]
 
 ########################################################################
 
 ##				Determination of Luminosities 		      ##
 
 ########################################################################
-
 ##Calculate distance
 
 
@@ -148,64 +170,64 @@ D <- d*3.0857e24 #Distance (cm)
 ## H-alpha luminosity
 
 print('Determining H-alpha luminosity.')
-La1 <- 4*pi*D^2*fa*2.3504e-11 #Luminosity [erg s^-1 ].
+LA1 <- 4*pi*D^2*fa*2.3504e-11 #Luminosity [erg s^-1 ].
 
 ## H-beta Luminosity
 
 print('Determining H-beta luminosity.')
-Lb1 <- 4*pi*D^2*fb*2.3504e-11 #Luminosity [erg s^-1 ].
+LB1 <- 4*pi*D^2*fb*2.3504e-11 #Luminosity [erg s^-1 ].
 
 ## [O III] 4959 luminosity
 
 print('Determining [O III] 4959 luminosity.')
-Lo11 <- 4*pi*D^2*fo1*2.3504e-11 #Luminosity [erg s^-1 ].
+LO11 <- 4*pi*D^2*fo1*2.3504e-11 #Luminosity [erg s^-1 ].
 
 ## [O III] 5007 luminosity
 
 print('Determining [O III] 5007 luminosity.')
-Lo1 <- 4*pi*D^2*fo*2.3504e-11 #Luminosity [erg s^-1 ].
+LO1 <- 4*pi*D^2*fo*2.3504e-11 #Luminosity [erg s^-1 ].
 
 ## [N II] 6548
 
 print('Determining [N II] 6548 luminosity.')
-Ln11 <- 4*pi*D^2*fn1*2.3504e-11 #Luminosity [erg s^-1 ].
+LN11 <- 4*pi*D^2*fn1*2.3504e-11 #Luminosity [erg s^-1 ].
 
 ## [N II] 6583
 
 print('Determining [N II] 6583 luminosity.')
-Ln1 <- 4*pi*D^2*Fn*2.3504e-11 #Luminosity [erg s^-1 ].
+LN1 <- 4*pi*D^2*fn*2.3504e-11 #Luminosity [erg s^-1 ].
 
 # Fitering out NAs.
 
-La  <- La1[which(!is.na(La1))]
-Xa  <- X[which(!is.na(La1))]
-Ya  <- Y[which(!is.na(La1))]
-IDa <- ID2[which(!is.na(La1))]
+La  <- LA1[which(!is.na(LA1))]
+Xa  <- XA[which(!is.na(LA1))]
+Ya  <- YA[which(!is.na(LA1))]
+IDa <- IDA[which(!is.na(LA1))]
 
-Ln1  <- Ln11[which(!is.na(Ln11))]
-Xn1  <- X[which(!is.na(Ln11))]
-Yn1  <- Y[which(!is.na(Ln11))]
-IDn1 <- ID2[which(!is.na(Ln11))]
+Ln1  <- LN11[which(!is.na(LN11))]
+Xn1  <- XN1[which(!is.na(LN11))]
+Yn1  <- YN1[which(!is.na(LN11))]
+IDn1 <- IDN1[which(!is.na(LN11))]
 
-Ln  <- Ln1[which(!is.na(Ln1))]
-Xn  <- X[which(!is.na(Ln1))]
-Yn  <- Y[which(!is.na(Ln1))]
-IDn <- ID2[which(!is.na(Ln1))]
+Ln  <- LN1[which(!is.na(LN1))]
+Xn  <- XN[which(!is.na(LN1))]
+Yn  <- YN[which(!is.na(LN1))]
+IDn <- IDN[which(!is.na(LN1))]
 
-Lb  <- Lb1[which(!is.na(Lb1))]
-Xb  <- X[which(!is.na(Lb1))]
-Yb  <- Y[which(!is.na(Lb1))]
-IDb <- ID2[which(!is.na(Lb1))]
+Lb  <- LB1[which(!is.na(LB1))]
+Xb  <- XB[which(!is.na(LB1))]
+Yb  <- YB[which(!is.na(LB1))]
+IDb <- IDB[which(!is.na(LB1))]
 
-Lo1  <- Lo11[which(!is.na(Lo11))]
-Xo1  <- X[which(!is.na(Lo11))]
-Yo1  <- Y[which(!is.na(Lo11))]
-IDo1 <- ID2[which(!is.na(Lo11))]
+Lo1  <- LO11[which(!is.na(LO11))]
+Xo1  <- XO1[which(!is.na(LO11))]
+Yo1  <- YO1[which(!is.na(LO11))]
+IDo1 <- IDO1[which(!is.na(LO11))]
 
-Lo  <- Lo1[which(!is.na(Lo1))]
-Xo  <- X[which(!is.na(Lo1))]
-Yo  <- Y[which(!is.na(Lo1))]
-IDo <- ID2[which(!is.na(Lo1))]
+Lo  <- LO1[which(!is.na(LO1))]
+Xo  <- XO[which(!is.na(LO1))]
+Yo  <- YO[which(!is.na(LO1))]
+IDo <- IDO[which(!is.na(LO1))]
 
 
 ##########################################################################
@@ -233,22 +255,23 @@ print('Plotting H-alpha luminosity map.')
 ## Specify color map for z.
 
 rbPal <-  colorRampPalette((rainbow(100,start=0,end=0.9))) 
-Col <- rbPal(100)[as.numeric(cut(log10(La[which(La >= 1e25)]),breaks = 100))]
+Col <- rbPal(100)[as.numeric(cut(log10(La[which(La >= 1e35)]),breaks = 100))]
 
-plot(Xa[which(La >= 1e25)],Ya[which(La >= 1e25)],xlim=c(-40,40),ylim=c(-40,40),xaxs= "i",yaxs="i", col="white", main = expression(paste(galaxy[i]," H",alpha," luminosity")),xlab = expression(paste(Delta, alpha," (arcsec)")), ylab = expression(paste(Delta, delta," (arcsec)")), cex.lab=1.3, cex.axis=1.3)
+title <- str_c(galaxy[i]," H-alpha luminosity")
+plot(Xa[which(La >= 1e35)],Ya[which(La >= 1e35)],xlim=c(-40,40),ylim=c(-40,40),xaxs= "i",yaxs="i", col="white", main = title,xlab = expression(paste(Delta, alpha," (arcsec)")), ylab = expression(paste(Delta, delta," (arcsec)")), cex.lab=1.3, cex.axis=1.3)
 #rasterImage(ima,-40,-40,40,40,interpolate=F)
-points(Xa[which(La >= 1e25)],Ya[which(La >= 1e25)], col = (Col), pch=15,cex = 0.7) #Plots spaxels with color map reversed (to have greater values darker and lower lighter).
+points(Xa[which(La >= 1e35)],Ya[which(La >= 1e35)], col = (Col), pch=15,cex = 0.7) #Plots spaxels with color map reversed (to have greater values darker and lower lighter).
 grid() #Coordinates'grid.
 abline(h = 0, v = 0, col = "gray60",lwd=2)
 #mtext(side=3,expression(paste(" log"[10]," H",alpha," Luminosity [erg s"^-1,"]")))
-image.plot( -40, -40, legend.only=TRUE, zlim= range(log10(La[which(La >= 1e25)])), col = ((rainbow(100,start=0,end=0.9)))) #Plots the color bar. Reverse colors here too.
+image.plot( -40, -40, legend.only=TRUE, zlim= range(log10(La[which(La >= 1e35)])), col = ((rainbow(100,start=0,end=0.9)))) #Plots the color bar. Reverse colors here too.
 
 #Saving plot.
 
-#map <- str_c("/data2/CALIFA/",galaxy[i],".COMB/",galaxy[i],"_Luminosity_Ha.eps")
-#dev.copy2eps(file=map)
-#map <- str_c("convert -density 300 /data2/CALIFA/",galaxy[i],".COMB/",galaxy[i],"_Luminosity_Ha.eps /data2/CALIFA/",galaxy[i],".COMB/",galaxy[i],"_Luminosity_Ha.png")
-#system(map)
+map <- str_c(galaxy[i],"/",galaxy[i],"_Luminosity_Ha.eps")
+dev.copy2eps(file=map)
+map <- str_c("convert -density 300 ",galaxy[i],"/",galaxy[i],"_Luminosity_Ha.eps ",galaxy[i],"/",galaxy[i],"_Luminosity_Harms100.png")
+system(map)
 
 
 ##########################################################################
@@ -259,23 +282,24 @@ print('Plotting [O III] 5007 luminosity map.')
 ## Specify color map for z.
 
 rbPal <-  colorRampPalette((rainbow(100,start=0,end=0.9))) 
-Col <- rbPal(100)[as.numeric(cut(log10(Lo[which(Lo >= 1e25)]),breaks = 100))]
+Col <- rbPal(100)[as.numeric(cut(log10(Lo[which(Lo >= 1e35)]),breaks = 100))]
 
-
-plot(Xo[which(Lo >= 1e25)],Yo[which(Lo >= 1e25)],xlim=c(-40,40),ylim=c(-40,40),xaxs= "i",yaxs="i", col="white", main = expression(paste(galaxy[i]," [O III] ",lambda,"5007 luminosity")),xlab = expression(paste(Delta, alpha," (arcsec)")), ylab = expression(paste(Delta, delta," (arcsec)")), cex.lab=1.3, cex.axis=1.3)
+title <- str_c(galaxy[i]," [O III] 5007 luminosity")
+plot(Xo[which(Lo >= 1e35)],Yo[which(Lo >= 1e35)],xlim=c(-40,40),ylim=c(-40,40),xaxs= "i",yaxs="i", col="white", main = title,xlab = expression(paste(Delta, alpha," (arcsec)")), ylab = expression(paste(Delta, delta," (arcsec)")), cex.lab=1.3, cex.axis=1.3)
 #rasterImage(ima,-40,-40,40,40,interpolate=F)
-points(Xo[which(Lo >= 1e25)],Yo[which(Lo >= 1e25)], col = (Col), pch=15,cex = 0.7) #Plots spaxels with color map reversed (to have greater values darker and lower lighter).
+points(Xo[which(Lo >= 1e35)],Yo[which(Lo >= 1e35)], col = (Col), pch=15,cex = 0.7) #Plots spaxels with color map reversed (to have greater values darker and lower lighter).
 grid() #Coordinates'grid.
 abline(h = 0, v = 0, col = "gray60",lwd=2)
 #mtext(side=3, expression(paste(" log"[10]," [O III] ",lambda,"5007 Luminosity [erg s"^-1,"]")))
-image.plot( -40, -40, legend.only=TRUE, zlim= range(log10(Lo[which(Lo >= 1e25)])), col = ((rainbow(100,start=0,end=0.9)))) #Plots the color bar. Reverse colors here too.
+image.plot( -40, -40, legend.only=TRUE, zlim= range(log10(Lo[which(Lo >= 1e35)])), col = ((rainbow(100,start=0,end=0.9)))) #Plots the color bar. Reverse colors here too.
 
 #Saving plot.
 
-#map <- str_c("/data2/CALIFA/",galaxy[i],".COMB/",galaxy[i],"_Luminosity_OIII.eps")
-#dev.copy2eps(file=map)
-#map <- str_c("convert -density 300 /data2/CALIFA/",galaxy[i],".COMB/",galaxy[i],"_Luminosity_OIII.eps /data2/CALIFA/",galaxy[i],".COMB/",galaxy[i],"_Luminosity_OIII.png")
-#system(map)
+map <- str_c(galaxy[i],"/",galaxy[i],"_Luminosity_OIII5007.eps")
+dev.copy2eps(file=map)
+map <- str_c("convert -density 300 ",galaxy[i],"/",galaxy[i],"_Luminosity_OIII5007.eps ",galaxy[i],"/",galaxy[i],"_Luminosity_OIII5007rms100.png")
+system(map)
+
 
 ##########################################################################
 ##[N II] 6584  Map
@@ -285,22 +309,24 @@ print('Plotting [N II] 6583 luminosity map.')
 ## Specify color map for z.
 
 rbPal <-  colorRampPalette((rainbow(100,start=0,end=0.9))) 
-Col <- rbPal(100)[as.numeric(cut(log10(Ln[which(Ln >= 1e25)]),breaks = 100))]
+Col <- rbPal(100)[as.numeric(cut(log10(Ln[which(Ln >= 1e35)]),breaks = 100))]
 
-plot(Xn[which(Ln >= 1e25)],Yn[which(Ln >= 1e25)],xlim=c(-40,40),ylim=c(-40,40),xaxs= "i",yaxs="i", col="white", main=expression(paste(galaxy[i]," [N II] ",lambda,"6583 luminosity")), xlab = expression(paste(Delta, alpha," (arcsec)")), ylab = expression(paste(Delta, delta," (arcsec)")), cex.lab=1.3, cex.axis=1.3)
+title <- str_c(galaxy[i]," [N II] 6583 luminosity")
+plot(Xn[which(Ln >= 1e35)],Yn[which(Ln >= 1e35)],xlim=c(-40,40),ylim=c(-40,40),xaxs= "i",yaxs="i", col="white", main= title, xlab = expression(paste(Delta, alpha," (arcsec)")), ylab = expression(paste(Delta, delta," (arcsec)")), cex.lab=1.3, cex.axis=1.3)
 #rasterImage(ima,-40,-40,40,40,interpolate=F)
-points(Xn[which(Ln >= 1e25)],Yn[which(Ln >= 1e25)], col = (Col), pch=15,cex = 0.7) #Plots spaxels with color map reversed (to have greater values darker and lower lighter).
+points(Xn[which(Ln >= 1e35)],Yn[which(Ln >= 1e35)], col = (Col), pch=15,cex = 0.7) #Plots spaxels with color map reversed (to have greater values darker and lower lighter).
 grid() #Coordinates'grid.
 abline(h = 0, v = 0, col = "gray60",lwd=2)
 #mtext(side=3,expression(paste(" log"[10]," [N II] ",lambda," 6584 Luminosity [erg s"^-1,"]")))
-image.plot( -40, -40, legend.only=TRUE, zlim= range(log10(Ln[which(Ln >= 1e25)])), col = ((rainbow(100,start=0,end=0.9)))) #Plots the color bar. Reverse colors here too.
+image.plot( -40, -40, legend.only=TRUE, zlim= range(log10(Ln[which(Ln >= 1e35)])), col = ((rainbow(100,start=0,end=0.9)))) #Plots the color bar. Reverse colors here too.
 
 #Saving plot.
 
-#map <- str_c("/data2/CALIFA/",galaxy[i],".COMB/",galaxy[i],"_Luminosity_NII.eps")
-#dev.copy2eps(file=map)
-#map <- str_c("convert -density 300 /data2/CALIFA/",galaxy[i],".COMB/",galaxy[i],"_Luminosity_NII.eps /data2/CALIFA/",galaxy[i],".COMB/",galaxy[i],"_Luminosity_NII.png")
-#system(map)
+map <- str_c(galaxy[i],"/",galaxy[i],"_Luminosity_NII6583.eps")
+dev.copy2eps(file=map)
+map <- str_c("convert -density 300 ",galaxy[i],"/",galaxy[i],"_Luminosity_NII6583.eps ",galaxy[i],"/",galaxy[i],"_Luminosity_NII6583rms100.png")
+system(map)
+
 
 ##########################################################################
 ##H-alpha equivalent width
